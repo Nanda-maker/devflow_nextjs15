f4470458645e4dd35efafd313ef1dc3e2182bf36
Nanda-maker <nandakrgurung123@gmail.com>
2024-11-07 20:17:52 +0600

    ESLint and Prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 const Home = () => (
-  <h1 className="text-3xl text-violet-700 font-black">nextjs 15</h1>
+  <h1 className="text-3xl text-white font-black">
+    Welcome to the world of Nextjs 15
+  </h1>
 );
 export default Home;
