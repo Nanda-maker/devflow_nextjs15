625efd1b8d49fcaf84a6cb7cac0c44374c088f03
Nanda-maker <nandakrgurung123@gmail.com>
2025-07-25 13:36:00 +0600

    github authentication and authroization session with login and logout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
 
 import ThemeProvider from "@/context/Theme";
+import { Toaster } from "@/components/ui/sonner";
+
+import { auth } from "@/auth";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -25,25 +30,27 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+  const session = await auth();
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+      <SessionProvider session={session}>
+        <body
+          className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
         >
-          {children}
-        </ThemeProvider>
-      </body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+          <Toaster />
+        </body>
+      </SessionProvider>
     </html>
   );
-}
+};
+
+export default RootLayout;
--- components/forms/SocialAuthForm.tsx
+++ components/forms/SocialAuthForm.tsx
@@ -1,19 +1,35 @@
+"use client";
+
 import React from "react";
 import { Button } from "../ui/button";
 import Image from "next/image";
+import { toast } from "sonner";
+import { signIn } from "next-auth/react"; // Importing the signIn function from auth.ts
+import ROUTES from "@/constants/routes";
 
 const SocialAuthForm = () => {
   const buttonClass =
     "background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5";
-  const handlesignIn = async (provider: "google" | "github") => {
+  const handleSignIn = async (provider: "google" | "github") => {
     try {
+      await signIn(provider, {
+        redirectTo: ROUTES.HOME,
+        //redirect: false,
+      });
+      //   throw new Error("Sign-in not implemented yet");
     } catch (error) {
       console.log(error);
+      toast.error("Sign-in Failed", {
+        description:
+          error instanceof Error
+            ? error.message
+            : "An error occurred during sign-in.",
+      });
     }
   };
   return (
     <div className="mt-10 flex flex-wrap gap-2.5">
-      <Button className={buttonClass}>
+      <Button className={buttonClass} onClick={() => handleSignIn("github")}>
         <Image
           src="/icons/github.svg"
           alt="GitHub Logo"
@@ -23,7 +39,15 @@ const SocialAuthForm = () => {
         />
         <span>Log in with GitHub</span>
       </Button>
-      <Button className={buttonClass}>
+      {/* <form
+        action={async () => {
+          "use server";
+          await signIn();
+        }}
+      >
+        <button type="submit">Sign in</button>
+      </form> */}
+      <Button className={buttonClass} onClick={() => handleSignIn("google")}>
         <Image
           src="/icons/google.svg"
           alt="Google Logo"
