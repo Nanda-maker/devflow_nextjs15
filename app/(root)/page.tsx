33b583f801825741fd0fab494486294ae88fa259
Nanda-maker <nandakrgurung123@gmail.com>
2025-07-31 19:08:53 +0600

    Implement the leftsidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,16 @@
 import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+//import { Button } from "@/components/ui/button";
+//import ROUTES from "@/constants/routes";
 
 const Home = async () => {
   const session = await auth();
-  console.log("Session:", session);
+  // console.log("Session:", session);
   return (
     <div>
       <h1 className="h1-bold font-space-grotesk">
         Welcome to the world of Next.js
       </h1>
-      <form
+      {/* <form
         className="px-10 pt-[100px]"
         action={async () => {
           "use server";
@@ -18,7 +18,7 @@ const Home = async () => {
         }}
       >
         <Button type="submit">Log out</Button>
-      </form>
+      </form> */}
     </div>
   );
 };
--- components/navigation/navbar/MobileNavigation.tsx
+++ components/navigation/navbar/MobileNavigation.tsx
@@ -54,7 +54,7 @@ const MobileNavigation = () => {
           <div className="flex flex-col gap-3">
             <SheetClose asChild>
               <Link href={ROUTES.SIGN_IN}>
-                <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                <Button className="cursor-pointer small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
                   <span className="primary-text-gradient">Log In</span>
                 </Button>
               </Link>
@@ -62,7 +62,7 @@ const MobileNavigation = () => {
 
             <SheetClose asChild>
               <Link href={ROUTES.SIGN_UP}>
-                <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
+                <Button className="cursor-pointer small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
                   Sign Up
                 </Button>
               </Link>
