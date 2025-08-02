7575ff32992a984cb34d1d2279216c99979fb6fe
Nanda-maker <nandakrgurung123@gmail.com>
2025-08-02 16:59:15 +0600

    implement local search bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,65 @@
-import { auth, signOut } from "@/auth";
+//import { auth, signOut } from "@/auth";
 //import { Button } from "@/components/ui/button";
 //import ROUTES from "@/constants/routes";
+import Link from "next/link";
+import LocalSearch from "@/components/search/LocalSearch";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
 
-const Home = async () => {
-  const session = await auth();
+const questions = [
+  {
+    _id: "1",
+    title: "How to learn React?",
+    description: "I want to learn React, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "JavaScript" },
+    ],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+  {
+    _id: "2",
+    title: "How to learn JavaScript?",
+    description: "I want to learn JavaScript, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "JavaScript" },
+    ],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+];
+interface SearchParams {
+  searchParams?: Promise<{ [key: string]: string }>;
+}
+const Home = async ({ searchParams }: SearchParams) => {
+  // const session = await auth();
   // console.log("Session:", session);
+  const { query = "" } = (await searchParams) || {};
+
+  const filteredQuestions = questions.filter((question) =>
+    question.title.toLowerCase().includes(query?.toLowerCase())
+  );
+
   return (
-    <div>
-      <h1 className="h1-bold font-space-grotesk">
-        Welcome to the world of Next.js
-      </h1>
-      {/* <form
+    <>
+      <section className="flex w-full flex-col-reverse sm:flex-row justify-between gap-4 sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+
+        {/* <form
         className="px-10 pt-[100px]"
         action={async () => {
           "use server";
@@ -19,7 +68,22 @@ const Home = async () => {
       >
         <Button type="submit">Log out</Button>
       </form> */}
-    </div>
+      </section>
+      <section className="mt-11">
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />
+      </section>
+      {/* homefilter */}
+      <div className="mt-10 flex flex-col w-full gap-6">
+        {filteredQuestions.map((question) => (
+          <h1 key={question._id}>{question.title}</h1>
+        ))}
+      </div>
+    </>
   );
 };
 
